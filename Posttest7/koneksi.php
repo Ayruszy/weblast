e6a4cbcfd9daf249fe8525b00d546e6577d13b91
Ayruszy <145456856+Ayruszy@users.noreply.github.com>
2023-11-01 19:28:00 +0800

    Delete Posttest7 directory

--- Posttest7/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $server = "localhost"; //xampp
-    $user = "root"; //selalu root
-    $password = ""; 
-    $db_name = "post5";
-
-    $conn = mysqli_connect ($server, $user, $password, $db_name);
-
-?>
